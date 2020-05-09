1090d1529ab29e66686307e67c453e37aeaf4108
alliocha1805 <alexis.piloy@gmail.com>
2020-05-09 15:35:21 +0200

    Passage en FR + divers modifs

--- collabio/collab/admin.py
+++ collabio/collab/admin.py
@@ -3,14 +3,26 @@ from django.contrib import admin
 # Register your models here.
 from .models import competences, familleCompetences, outils, familleOutils, collaborateurs, experiences, domaine, client, projet
 
-admin.site.register(competences)
+class CompetenceAdmin(admin.ModelAdmin):
+    search_fields = ['nomCompetence']
+    list_filter = ('famille',)
+admin.site.register(competences, CompetenceAdmin)
 admin.site.register(familleCompetences)
-admin.site.register(outils)
+class OutilAdmin(admin.ModelAdmin):
+    search_fields = ['nomOutil']
+    list_filter = ('famille',)
+admin.site.register(outils, OutilAdmin)
 admin.site.register(familleOutils)
-admin.site.register(collaborateurs)
+class CollabAdmin(admin.ModelAdmin):
+    list_filter = ('estEnIntercontrat',)
+admin.site.register(collaborateurs, CollabAdmin)
 class ExpeAdmin(admin.ModelAdmin):
     search_fields = ['collaborateurMission__nomCollaborateur','collaborateurMission__prenomCollaborateur']
+    list_filter = ('client','collaborateurMission__nomCollaborateur')
 admin.site.register(experiences, ExpeAdmin)
-admin.site.register(projet)
+class ProjetAdmin(admin.ModelAdmin):
+    search_fields = ['experiencesLiees__client__nomClient','nomProjet']
+    list_filter = ('experiencesLiees__client__nomClient',)
+admin.site.register(projet, ProjetAdmin)
 admin.site.register(domaine)
 admin.site.register(client)
